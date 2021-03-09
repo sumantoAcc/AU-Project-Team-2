5689029ec680ca46a7be00e20b99d81e216c2a65
Nisarg Shah <Nisarg.Shah@accolitedigital.com>
2021-03-09 14:33:54 +0530

    Added get and add Comment API

--- Backend/src/main/java/com/au/discussionforum/dao/CommentRepository.java
+++ Backend/src/main/java/com/au/discussionforum/dao/CommentRepository.java
@@ -1,5 +1,13 @@
 package com.au.discussionforum.dao;
 
-public class CommentRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.au.discussionforum.model.Comment;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long>{
+	List<Comment> findByAnswerAnsId(int ansId);
 }
